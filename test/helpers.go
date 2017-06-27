e45db7771979f7b3a86b44e4e1d07db445ab8518
Jean Mertz <jean@mertz.fm>
2017-06-27 09:02:47 +0200

    document Kafka test flag

--- test/helpers.go
+++ test/helpers.go
@@ -2,6 +2,13 @@ package test
 
 import "flag"
 
+// Kafka is set to `true` if `go test` is run with the `-kafka` flag.
+//
+// Use this to by-default skip tests that depend on a running Kafka instance:
+//
+//   if !*test.Kafka {
+//     t.Skip()
+//   }
 var Kafka *bool
 
 func init() {
