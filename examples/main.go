b640e011c0f6d76170f82e2e750dd91d30338d11
Jean Mertz <jean@mertz.fm>
2017-07-08 00:32:52 +0200

    Tweak/explain example

--- examples/main.go
+++ examples/main.go
@@ -6,18 +6,28 @@ import (
 )
 
 func main() {
+	// Instantiate a new consumer and producer.
 	consumer, producer, err := streamclient.NewConsumerAndProducer()
 	if err != nil {
 		panic(err)
 	}
 
+	// Don't forget to close the producer once you're done.
 	defer producer.Close()
 
 	for msg := range consumer.Messages() {
+		// Initialize a new message.
 		message := &stream.Message{}
 
+		// Set the value of the message to the consumed message, prepended with
+		// "processed: ".
 		message.Value = append([]byte("processed: "), msg.Value...)
 
+		// Send message to the configured producer.
 		producer.Messages() <- message
+
+		// Mark the consumed message as "done". This lets the client know it should
+		// not retry this message if the application is restarted.
+		msg.Done()
 	}
 }
