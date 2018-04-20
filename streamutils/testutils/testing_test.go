71f75b84b72512e14ba3a79602c5589990c85a16
Jean Mertz <jean@mertz.fm>
2018-04-20 22:25:24 +0200

    Add `testutils.TestIntegration` and `Random` tests

--- streamutils/testutils/testing_test.go
+++ streamutils/testutils/testing_test.go
@@ -1,6 +1,7 @@
 package testutils_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/blendle/go-streamprocessor/streamutils/testutils"
@@ -9,4 +10,14 @@ import (
 
 func TestTimeoutMultiplier(t *testing.T) {
 	assert.Equal(t, 1, testutils.TimeoutMultiplier)
+}
+
+func TestIntegration_Test(t *testing.T) {
+	testutils.Integration(t)
+}
+
+func TestRandom(t *testing.T) {
+	s := testutils.Random(t)
+
+	assert.True(t, strings.HasPrefix(s, "TestRandom-"), s)
 }
